7ab4dad22619cd42b4594ce91df9218ae0618ffd
boldak <boldak.andrey@gmail.com>
2018-02-24 15:45:51 +0200

    add run with file

--- api/controllers/DPSController.js
+++ api/controllers/DPSController.js
@@ -11,30 +11,68 @@ logger.debug("Start DJ Storage Service")
 module.exports = {
 
     run: function(req, resp) {
+        
+        // console.log("RUN")
+        var host, script, locale, state;
 
-        var script = req.body.script;
-        var state = req.body.state;
-        var locale = req.body.locale || "en";
-        let host = req.host+":"+req.port;
-        locale = (locale == "uk") ? "ua" : locale;
+        req.file('file').upload({}, (err, uploadedFiles) => {
+            
+            if (uploadedFiles.length>0) {
+                
+                let file = uploadedFiles[0]
+                let fileContent = require("fs").readFileSync(file.fd)
+                // let config = JSON.parse(req.body.config)
+                
+                script = JSON.parse(req.body.script);
+                state =  JSON.parse(req.body.state)
+                locale = JSON.parse(req.body.locale) || "en";
+                host = req.host+":"+req.port;
+                locale = (locale == "uk") ? "ua" : locale;
+                state = (state) || {
+                    locale : locale 
+                }
+                state.client = JSON.parse(req.body.client);
+                state.storage.$file = fileContent;
+            
+            } else {
 
-        state = (state) || {
-            locale : locale 
-        }
-        state.client = req.body.client;
-        // console.log(req.host, script, state)
-        var executable = new Script()
-            .host(host)
-            .config(conf)
-            .script(script)
-            // .state(state)
-            .run(state)
-            .then(function(result) {
-                resp.send(result)
-            })
-            .catch(function(error) {
-                resp.send({ type: "error", data: error })
-            })
+                script = req.body.script;
+                state = req.body.state;
+                locale = req.body.locale || "en";
+                host = req.host+":"+req.port;
+                locale = (locale == "uk") ? "ua" : locale;
+
+                state = (state) || {
+                    locale : locale 
+                }
+                state.client = req.body.client;
+            
+            }
+            
+            // console.log("host", host)
+            // console.log("script", script)
+            // console.log("state", state)
+
+
+            // console.log(req.host, script, state)
+            var executable = new Script()
+                .host(host)
+                .config(conf)
+                .script(script)
+                // .state(state)
+                .run(state)
+                .then(function(result) {
+                    resp.send(result)
+                })
+                .catch(function(error) {
+                    resp.send({ type: "error", data: error })
+                })
+            })    
+            // var file = uploadedFiles[0];
+
+
+
+        
     }
 
 }
